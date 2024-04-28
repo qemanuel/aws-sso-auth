c1930127009048842c2d2f5c6be10f0f71c71338
Emanuel Quiroga <emanuel@xlabs.xyz>
2024-04-28 16:14:03 -0300

    add main.py

---
+++ main.py
@@ -0,0 +1,28 @@
+import os
+from dotenv import load_dotenv
+from SSOConfig import SSOConfig
+
+def main():
+    load_dotenv()
+    startUrl = os.getenv('SSO_START_URL')
+    ssoRoleName = os.getenv('SSO_ROLE_NAME')
+    ssoRegion = os.getenv('SSO_REGION')
+    defaultRegion = os.getenv('DEFAULT_REGION')
+    defaultOutput = os.getenv('DEFAULT_OUTPUT', 'json')
+    fileFormat = os.getenv('FILE_FORMAT')
+    filePath = os.getenv('FILE_PATH', 'config')
+
+    for key in startUrl,ssoRoleName,ssoRegion,defaultRegion:
+        if(key == None):
+            raise Exception(f'Environment Variable {key} is missing. Please, verify your .env file')
+
+    ssoConfig = SSOConfig(startUrl, ssoRoleName, ssoRegion, defaultRegion, defaultOutput)
+    if fileFormat == "config" :
+        ssoConfig.getConfigProfiles(filePath)
+    elif fileFormat == "credentials":
+        ssoConfig.getCredentialsProfiles(filePath)
+    else:
+        raise Exception(f'Environment Variable "FILE_FORMAT" must be either "config" or "credentials"')
+
+if __name__ == "__main__":
+    main()
